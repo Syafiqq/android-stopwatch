876c3e7ca2f026a7823f264bb3465a51d33cea47
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-24 20:06:40 +0700

    Reformat Code

--- stopwatch-core/src/main/java/com/danielbostwick/stopwatch/core/model/models.kt
+++ stopwatch-core/src/main/java/com/danielbostwick/stopwatch/core/model/models.kt
@@ -3,10 +3,7 @@ package com.danielbostwick.stopwatch.core.model
 import org.joda.time.DateTime
 import org.joda.time.Duration
 
-data class Stopwatch(
-        val state: StopwatchState,
-        val startedAt: DateTime,
-        val offset: Duration)
+data class Stopwatch(val state: StopwatchState, val startedAt: DateTime, val offset: Duration)
 
 enum class StopwatchState
 { PAUSED, STARTED }
