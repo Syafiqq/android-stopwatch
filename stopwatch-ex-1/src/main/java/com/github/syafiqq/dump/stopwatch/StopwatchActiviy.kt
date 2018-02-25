308cd9622246e09883f685da20dedb7ca2858341
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-02-25 14:49:17 +0700

    Fix Activity

--- stopwatch-ex-1/src/main/java/com/github/syafiqq/dump/stopwatch/StopwatchActiviy.kt
+++ stopwatch-ex-1/src/main/java/com/github/syafiqq/dump/stopwatch/StopwatchActiviy.kt
@@ -2,13 +2,16 @@ package com.github.syafiqq.dump.stopwatch
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import timber.log.Timber
 
 class StopwatchActiviy: AppCompatActivity()
 {
 
-    override fun onCreate(savedInstanceState: Bundle?)
+    override fun onCreate(state: Bundle?)
     {
-        super.onCreate(savedInstanceState)
+        Timber.d("onCreate [$state]")
+
+        super.onCreate(state)
         setContentView(R.layout.activity_stopwatch_activiy)
     }
 }
