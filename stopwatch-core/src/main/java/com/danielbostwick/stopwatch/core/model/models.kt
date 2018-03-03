e0799b7a13c309da8933bd0ef737ff66f9a32283
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-04 05:44:42 +0700

    Mark property as mutable

--- stopwatch-core/src/main/java/com/danielbostwick/stopwatch/core/model/models.kt
+++ stopwatch-core/src/main/java/com/danielbostwick/stopwatch/core/model/models.kt
@@ -3,7 +3,7 @@ package com.danielbostwick.stopwatch.core.model
 import org.joda.time.DateTime
 import org.joda.time.Duration
 
-data class Stopwatch(val state: StopwatchState = StopwatchState.PAUSED, val startedAt: DateTime = DateTime.now(), val offset: Duration = Duration.ZERO)
+data class Stopwatch(var state: StopwatchState = StopwatchState.PAUSED, var startedAt: DateTime = DateTime.now(), var offset: Duration = Duration.ZERO)
 
 enum class StopwatchState
 { PAUSED, STARTED }
