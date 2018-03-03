e4b4fd6f6a20709317268a267d039c76ebfc266a
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-04 05:45:03 +0700

    Mark Clock As Serializable

--- stopwatch-core/src/main/java/com/danielbostwick/stopwatch/core/model/models.kt
+++ stopwatch-core/src/main/java/com/danielbostwick/stopwatch/core/model/models.kt
@@ -2,8 +2,10 @@ package com.danielbostwick.stopwatch.core.model
 
 import org.joda.time.DateTime
 import org.joda.time.Duration
+import java.io.Serializable
 
-data class Stopwatch(var state: StopwatchState = StopwatchState.PAUSED, var startedAt: DateTime = DateTime.now(), var offset: Duration = Duration.ZERO)
+data class Stopwatch(var state: StopwatchState = StopwatchState.PAUSED, var startedAt: DateTime = DateTime.now(), var offset: Duration = Duration.ZERO):
+        Serializable
 
 enum class StopwatchState
 { PAUSED, STARTED }
