0b39254c99c8c234408df2f5a36a4b18360829d7
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-03-04 05:46:43 +0700

    Add set method

--- stopwatch-core/src/main/java/com/danielbostwick/stopwatch/core/model/models.kt
+++ stopwatch-core/src/main/java/com/danielbostwick/stopwatch/core/model/models.kt
@@ -6,6 +6,14 @@ import java.io.Serializable
 
 data class Stopwatch(var state: StopwatchState = StopwatchState.PAUSED, var startedAt: DateTime = DateTime.now(), var offset: Duration = Duration.ZERO):
         Serializable
+{
+    fun set(stopwatch: Stopwatch)
+    {
+        this.state = stopwatch.state
+        this.startedAt = stopwatch.startedAt
+        this.offset = stopwatch.offset
+    }
+}
 
 enum class StopwatchState
 { PAUSED, STARTED }
